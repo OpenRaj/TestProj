ea66809f57303c29057aa830eb44c018f39810ac
Somendra Kumar <somendrakumar08@gmail.com>
2020-07-04 17:50:29 -0700

    commiting GitTest2 into BranchX

---
+++ src/GitReposi/GitTest2.java
@@ -0,0 +1,10 @@
+package GitReposi;
+
+public class GitTest2 {
+
+	public static void main(String[] arg)
+	{
+		System.out.println("welcome to test-2");
+		System.out.println("Hello to test-2 Git Local Repository:)");
+	}
+}
