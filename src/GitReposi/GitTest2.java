c16c7035591be72ef68c5288b1ada362a1a12950
Somendra Kumar <somendrakumar08@gmail.com>
2020-07-05 07:05:37 -0700

    Added additional conditions as recommended.

--- src/GitReposi/GitTest2.java
+++ src/GitReposi/GitTest2.java
@@ -6,5 +6,6 @@ public class GitTest2 {
 	{
 		System.out.println("welcome to test-2");
 		System.out.println("Hello to test-2 Git Local Repository:)");
+		System.out.println("Hi I checked and added new conditions");
 	}
 }
